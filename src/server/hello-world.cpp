1cc6818065f510936faf61b2e1aaa1645fc87ea9
Justin Zhang <judtin@seas.upenn.edu>
2023-05-24 11:38:58 -0400

    Added boost library

--- src/server/hello-world.cpp
+++ src/server/hello-world.cpp
@@ -2,15 +2,24 @@
 #include "hello-greet.hpp"
 #include <iostream>
 #include <string>
+#include <boost/algorithm/string.hpp>
 
-// #include <boost/process.hpp>
+using namespace std;
 
 int main(int argc, char** argv) {
-  std::string who = "world";
+  string who = "world";
   if (argc > 1) {
     who = argv[1];
   }
-  std::cout << get_greet(who) << std::endl;
+
+  auto greet = get_greet(who);
+
+  cout << greet << endl;
+
+  // Check if the string starts with "Hello"
+  bool startsWithHello = boost::algorithm::starts_with(greet, "Hello");
+  cout << "Starts with \"Hello\": " << (startsWithHello ? "Yes" : "No") << endl;
+
   print_localtime();
 
   return 0;
