5065fbdeca042aad03373645118a1015ce7bbab9
Justin Zhang <judtin@seas.upenn.edu>
2024-04-16 02:26:26 -0400

    Test pre-commit

--- src/models.py
+++ src/models.py
@@ -6,9 +6,7 @@ from pydantic import BaseModel, ConfigDict
 
 
 class CustomModel(BaseModel):
-    model_config = ConfigDict(
-        populate_by_name=True,
-    )
+    model_config = ConfigDict(populate_by_name=True,)
 
     def serializable_dict(self, **kwargs):
         default_dict = self.model_dump()
