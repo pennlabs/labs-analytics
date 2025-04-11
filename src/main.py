b555391ea7a47313a4a6287d90d8708e3cd58ff1
Jesse Zong <63322084+dr-Jess@users.noreply.github.com>
2025-04-11 17:12:55 -0400

    Jessez/sentry setup (#42)
    * Update jwk verification pipeline. If this fixes backend-to-backend, we need to rework to double-auth for both potential users and other backends.
    
    * Update sentry stuff

--- src/main.py
+++ src/main.py
@@ -1,8 +1,19 @@
+import os
+
+import sentry_sdk
 from fastapi import Depends, FastAPI, HTTPException, Request
 
 from src.auth import verify_jwt
 from src.models import AnalyticsTxn
 from src.redis import set_redis_from_tx
+
+
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN", ""),
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 
 
 app = FastAPI()
